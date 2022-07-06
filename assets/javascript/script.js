f9587f58f59633f1010d8b43ffc1ceabf38bed85
Josh Hawk <hawkjosh@icloud.com>
2022-07-06 12:51:34 -0400

    Restructured time-blocks, moved from script.js to index.html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,42 +3,42 @@ let date = moment().format('dddd, MMMM Do');
 $('#currentDay').text(date);
 
 // Added timeblocks for standard business hours
-let schedule = $('.container');
-schedule.css('display', 'flex');
-schedule.css('flex-direction', 'column')
+// let schedule = $('.container');
+// schedule.css('display', 'flex');
+// schedule.css('flex-direction', 'column')
 
 // Created array of standard business hours
-let hours = [
-  '9AM  ',
-  '10AM',
-  '11AM',
-  '12PM',
-  '1PM ',
-  '2PM ',
-  '3PM ',
-  '4PM ',
-  '5PM ',
-]
+// let hours = [
+//   '9  ',
+//   '10 ',
+//   '11 ',
+//   '12 ',
+//   '1 ',
+//   '2 ',
+//   '3 ',
+//   '4 ',
+//   '5 ',
+// ]
 
 //Added <div> of labled row for each hour including input area for text and button to save
-for (i = 0; i < hours.length; i++) {
-  let hourLabel = $('<div>');
-  let hourInfo = $('<input>');
-  let saveBtn = $('<button>');
-  hourLabel.text(hours[i]);
-  hourLabel.addClass('hour row');
-  hourLabel.css('flex-direction', 'row')
-  hourLabel.css('justify-content', 'space-between');
-  hourInfo.addClass('time-block row');
-  hourInfo.css('text-align', 'left');
-  hourInfo.css('padding-left', '10px');
-  hourInfo.css('width', '80%')
-  hourInfo.css('align-self', 'flex-start');
-  saveBtn.text('Save');
-  saveBtn.addClass('time-block saveBtn');
-  saveBtn.css('align-self', 'stretch');
-  saveBtn.css('width', '10%');
-  schedule.append(hourLabel);
-  hourLabel.append(hourInfo);
-  hourLabel.append(saveBtn);
-}
+// for (i = 0; i < hours.length; i++) {
+//   let hourLabel = $('<div>');
+//   let hourInfo = $('<input>');
+//   let saveBtn = $('<button>');
+//   hourLabel.text(hours[i]);
+//   hourLabel.addClass('hour row');
+//   hourLabel.css('flex-direction', 'row')
+//   hourLabel.css('justify-content', 'space-between');
+//   hourInfo.addClass('time-block row');
+//   hourInfo.css('text-align', 'left');
+//   hourInfo.css('padding-left', '10px');
+//   hourInfo.css('width', '80%')
+//   hourInfo.css('align-self', 'flex-start');
+//   saveBtn.text('Save');
+//   saveBtn.addClass('time-block saveBtn');
+//   saveBtn.css('align-self', 'stretch');
+//   saveBtn.css('width', '10%');
+//   schedule.append(hourLabel);
+//   hourLabel.append(hourInfo);
+//   hourLabel.append(saveBtn);
+// }

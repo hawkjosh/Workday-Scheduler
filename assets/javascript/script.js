628e3529ad5924bbecd0b6f43967e23be953ba3f
Josh Hawk <hawkjosh@icloud.com>
2022-07-07 01:08:43 -0400

    Fixed issue with time-based color coding of time-blocks

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,21 +5,21 @@ $('#currentDay').text(date);
 let hour = document.querySelectorAll('.hour');
 let timeBlock = document.querySelectorAll('.time-block');
 
-console.log(hour[0].innerHTML);
-
+// Set number value to each hour in schedule, and display on HTML
 for (i=0; i<hour.length; i++) {
-  hour[i].textContent = moment(i+9, 'h').format('h A');
+  hour[i].value = moment(i+9, 'h').hour();
+  hour[i].innerHTML = moment(i+9, 'h').format('h A');
 }
 
 let currentTime = moment();
 
-// TODO → Something is not right below...all time-block elements are showing up as future regardless of the set time in that block
+// Conditional to color code time-blocks based on relationship to current time
 for (i=0; i<hour.length; i++) {
-  if (hour[i] < currentTime) {
-    timeBlock[i].classList.add('past');
-  } else if (hour[i] == currentTime) {
+  if (hour[i].value > currentTime.hour()) {
+    timeBlock[i].classList.add('future');
+  } else if (hour[i].value == currentTime.hour()) {
     timeBlock[i].classList.add('present');
   } else {
-    timeBlock[i].classList.add('future');
+    timeBlock[i].classList.add('past');
   }
 }
